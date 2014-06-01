e0e85f4e1b14236076efa635c738711d10c8bc2a
qsphan <dark2bright@gmail.com>
2014-06-01 22:56:31 +0100

    edit script

--- configEx.py
+++ configEx.py
@@ -2,12 +2,27 @@ import os
 import subprocess
 import glob
 
-oc = subprocess.Popen(['which', 'oc'], stdout=subprocess.PIPE).stdout.readline().rstrip()
-latte = subprocess.Popen(['which', 'count'], stdout=subprocess.PIPE).stdout.readline().rstrip()
+omega = 'symbolic.reliability.omegaPath=' + subprocess.Popen(['which', 'oc'], stdout=subprocess.PIPE).stdout.readline()
+latte = 'symbolic.reliability.lattePath=' + subprocess.Popen(['which', 'count'], stdout=subprocess.PIPE).stdout.readline()
+
+def editPath(conf):
+	with open(conf,'r') as f:
+		newlines = []
+		for line in f.readlines():
+			if 'omegaPath' in line:
+				line = omega
+			if 'lattePath' in line:
+				line = latte
+        		newlines.append(line)
+	with open(conf, 'w') as f:
+    		for line in newlines:
+        		f.write(line)
 
 curdir = os.getcwd()
 
 array_conf = glob.glob(curdir + '/src/examples/*.jpf')
 
 for conf in array_conf:
-	print conf
+	editPath (conf)
+
+
